252a21ff7f6bbf0cb1185b9bf9dac14424cab985
Carlos Steven Giraldo <143855227+Stevensgm@users.noreply.github.com>
2025-11-09 20:10:58 -0500

    Refactor Prototype pattern implementation
    correccion de codigo

--- PrototypeDemo.java
+++ PrototypeDemo.java
@@ -1,36 +1,33 @@
 ///patrón Prototype
 
-// Interfaz Prototype
-public interface Figura extends Cloneable {
-    Figura clonar();
-}
+public class PrototypeDemo {
 
-// Clase concreta que implementa Prototype
-public class Circulo implements Figura {
-    private int x;
-    private int y;
-    private String color;
-
-    public Circulo(int x, int y, String color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
+    interface Figura extends Cloneable {
+        Figura clonar();
     }
 
-    // Método para clonar el objeto
-    @Override
-    public Figura clonar() {
-        return new Circulo(x, y, color);
+    static class Circulo implements Figura {
+        private int x;
+        private int y;
+        private String color;
+
+        public Circulo(int x, int y, String color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+
+        @Override
+        public Figura clonar() {
+            return new Circulo(x, y, color);
+        }
+
+        @Override
+        public String toString() {
+            return "Círculo [x=" + x + ", y=" + y + ", color=" + color + "]";
+        }
     }
 
-    @Override
-    public String toString() {
-        return "Círculo [x=" + x + ", y=" + y + ", color=" + color + "]";
-    }
-}
-
-// Clase principal para probar Prototype
-public class Main {
     public static void main(String[] args) {
         Circulo original = new Circulo(10, 20, "rojo");
         Circulo copia = (Circulo) original.clonar();
@@ -39,3 +36,4 @@ public class Main {
         System.out.println("Copia: " + copia);
     }
 }
+
